ce217e2e8470a0adddcfac0ba10fe71738820ad1
Asrat <asratextras77@gmail.com>
2023-12-24 22:27:08 +0300

    initial commit

---
+++ responses/greetings.py
@@ -0,0 +1,23 @@
+greetings_responses = [
+    "Hello!",
+    "Hi there! How can I assist you today?",
+    "Hey, welcome! What can I help you with?",
+    "Greetings! How may I be of service?",
+    "Good day! How can I support you?",
+    "Hi! I'm here to help. What do you need assistance with?",
+    "Hello, and welcome! How can I assist you?",
+    "Hey, nice to see you! What brings you here?",
+    "Greetings and salutations! How may I assist you today?",
+    "Hi, it's great to have you! How can I help you?",
+    "Hello, wonderful to see you! What do you need help with today?",
+    "Hey there! How can I make your day better?",
+    "Hi, delighted to assist! What can I do for you?",
+    "Hello, and a warm welcome! How may I be of service today?",
+    "Hey, good to have you here! What brings you to us?",
+    "Hi, nice to meet you! How can I assist you today?",
+    "Hello! I'm here and ready to help. What can I do for you?",
+    "Hey, great to see you! How can I make your experience better?",
+    "Hi, welcome aboard! How can I assist you today?",
+    "Hello! I'm at your service. How can I help you?"
+]
+
